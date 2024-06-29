8a3853e36b49745ec6941226cc1171db5b5d88f4
Rahul <mrahulmaity623@gmail.com>
2024-06-29 12:27:31 +0530

    protection mechanism authcookie

--- Program.cs
+++ Program.cs
@@ -1,27 +1,39 @@
+using Microsoft.AspNetCore.DataProtection;
+using System.IO;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services.AddDataProtection();
 
 var app = builder.Build();
 
 
-app.MapGet("/username", (HttpContext ctx) =>
+app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
 {
     if (!ctx.Request.Cookies.TryGetValue("auth", out var authCookie))
     {
         return "no auth cookie found";
     }
 
-    var parts = authCookie.Split(':');
-    var key = parts[0];
-    var value = parts[1];
-    return value;
+    var protector = idp.CreateProtector("auth-cookie");
+
+   var unProtectedValue = protector.Unprotect(authCookie);
+    var parts = unProtectedValue.Split(':');
+
+    if (parts.Length != 2 || parts[0] != "usr")
+    {
+        return "invalid auth cookie";
+    }
+
+    var username = parts[1];
+    return username;
 });
 
 
-app.MapGet("/login", (HttpContext ctx) =>
+app.MapGet("/login", (HttpContext ctx,IDataProtectionProvider idp) =>
 {
-    ctx.Response.Headers["set-cookie"] = "auth=usr:anton";
+   var protector = idp.CreateProtector("auth-cookie");
+    ctx.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:anton")}";
     return "ok";
 });
 
