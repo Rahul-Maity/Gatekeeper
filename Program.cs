b92bae8771ed87e806a753ca2a03f1e81aa7fbfc
Rahul <mrahulmaity623@gmail.com>
2024-06-29 16:35:02 +0530

    structuring middleware and services

--- Program.cs
+++ Program.cs
@@ -1,39 +1,75 @@
 using Microsoft.AspNetCore.DataProtection;
 using System.IO;
+using System.Runtime.Intrinsics.Arm;
+using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDataProtection();
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddScoped<authService>();
 
 var app = builder.Build();
 
-
-app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
+app.Use(async
+    (ctx, next) =>
 {
+    var idp = ctx.RequestServices.GetRequiredService<IDataProtectionProvider>();
     if (!ctx.Request.Cookies.TryGetValue("auth", out var authCookie))
     {
-        return "no auth cookie found";
+        ctx.Response.StatusCode = 401;
+        ctx.Response.WriteAsync("NO auth cookie found");
+        return;
     }
 
     var protector = idp.CreateProtector("auth-cookie");
-
-   var unProtectedValue = protector.Unprotect(authCookie);
-    var parts = unProtectedValue.Split(':');
-
-    if (parts.Length != 2 || parts[0] != "usr")
+    try
     {
-        return "invalid auth cookie";
+        var unProtectedValue = protector.Unprotect(authCookie);
+        var parts = unProtectedValue.Split(':');
+
+        if (parts.Length != 2 || parts[0] != "usr")
+        {
+            ctx.Response.StatusCode = 401; // Unauthorized
+            await ctx.Response.WriteAsync("Invalid auth cookie");
+            return;
+        }
+        var key = parts[0];
+        var value = parts[1];
+
+        var claims = new List<Claim>();
+        claims.Add(new Claim(key, value));
+        var identity = new ClaimsIdentity(claims);
+        ctx.User = new ClaimsPrincipal(identity);
+
+
+
+
+    }
+    catch
+    {
+        ctx.Response.StatusCode = 401; // Unauthorized
+        await ctx.Response.WriteAsync("Invalid auth cookie");
+        return;
     }
 
-    var username = parts[1];
-    return username;
+
+    //ctx.User = new ClaimsPrincipal()
+    await next();
+    
+
+});
+
+app.MapGet("/username", (HttpContext ctx, IDataProtectionProvider idp) =>
+{
+
+    return ctx.User;
 });
 
 
-app.MapGet("/login", (HttpContext ctx,IDataProtectionProvider idp) =>
+app.MapGet("/login", (authService auth) =>
 {
-   var protector = idp.CreateProtector("auth-cookie");
-    ctx.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:anton")}";
+    auth.signin(); 
     return "ok";
 });
 
@@ -42,4 +78,24 @@ app.UseHttpsRedirection();
 
 
 app.Run();
+
+
+public class authService
+{
+    private readonly IHttpContextAccessor _accessor;
+    private readonly IDataProtectionProvider _idp;
+
+    public authService(IDataProtectionProvider idp, IHttpContextAccessor accessor)
+    {
+        _idp = idp;
+        _accessor = accessor;
+    }
+
+    public void signin()
+    {
+        var protector = _idp.CreateProtector("auth-cookie");
+        _accessor.HttpContext.Response.Headers["set-cookie"] = $"auth={protector.Protect("usr:anton")}";
+    }
+
+}
 
