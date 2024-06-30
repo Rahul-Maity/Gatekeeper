f9142fad6e6c45e2c7be30da0a8e1c0154e1bc28
Rahul <mrahulmaity623@gmail.com>
2024-06-30 17:41:47 +0530

    authorization

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
 using System.IO;
 using System.Runtime.Intrinsics.Arm;
@@ -7,8 +8,25 @@ using System.Security.Claims;
 var builder = WebApplication.CreateBuilder(args);
 
 const string authScheme = "cookie";
+
+const string authScheme2 = "cookie2";
+
 builder.Services.AddAuthentication(authScheme)
-    .AddCookie(authScheme);
+    .AddCookie(authScheme)
+    .AddCookie(authScheme2);
+
+
+builder.Services.AddAuthorization(builder =>
+{
+    builder.AddPolicy("eu passport", pb =>
+    {
+        pb.RequireAuthenticatedUser()
+        .AddAuthenticationSchemes(authScheme)
+        .AddRequirements()
+        .RequireClaim("passport_type", "eur");
+    });
+    
+});
 
 var app = builder.Build();
 
@@ -16,10 +34,45 @@ var app = builder.Build();
 
 app.UseAuthentication();
 
+app.UseAuthorization();
+
+
+
+//app.Use((ctx, next) =>
+//{
+
+//    if (ctx.Request.Path.StartsWithSegments("/login"))
+//    {
+//        return next();
+//    }
+//    //if(ctx.Request.Path.StartsWithSegments("/favicon.ico"))
+//    //{
+//    //    ctx.Response.StatusCode = 204;
+//    //    return next();
+
+//    //}
+//    if (!ctx.User.Identities.Any(x => x.AuthenticationType == authScheme))
+//    {
+//        ctx.Response.StatusCode = 401;
+//        return Task.CompletedTask;
+//    }
+
+
+//    if (!ctx.User.HasClaim("passport_type", "eur"))
+//    {
+//        ctx.Response.StatusCode = 403;
+//        return Task.CompletedTask;
+//    }
+
+//    return next();
+//});
+
+
+
 app.MapGet("/username", (HttpContext ctx) =>
 {
 
-    return ctx.User.FindFirst("usr").Value ?? "empty";
+  return ctx.User.FindFirst("usr").Value ?? "empty";
 });
 
 
@@ -27,22 +80,28 @@ app.MapGet("/username", (HttpContext ctx) =>
 
 app.MapGet("/sweden", (HttpContext ctx) =>
 {
-    if(!ctx.User.Identities.Any(x => x.AuthenticationType == authScheme))
-    {
-        ctx.Response.StatusCode = 401;
-        return "";
-    }
+    //if(!ctx.User.Identities.Any(x => x.AuthenticationType == authScheme))
+    //{
+    //    ctx.Response.StatusCode = 401;
+    //    return "";
+    //}
 
-    if(!ctx.User.HasClaim("passport_type","eur"))
-    {
-        ctx.Response.StatusCode = 403;
-        return "";
-    }
+    //if(!ctx.User.HasClaim("passport_type","eur"))
+    //{
+    //    ctx.Response.StatusCode = 403;
+    //    return "";
+    //}
 
     return "allowed";
 
    
-});
+}).RequireAuthorization("eu passport"); ;
+
+
+
+
+
+
 
 
 app.MapGet("/login",async (HttpContext ctx) =>
@@ -55,13 +114,30 @@ app.MapGet("/login",async (HttpContext ctx) =>
     var user = new ClaimsPrincipal(identity);
     await ctx.SignInAsync(authScheme,user);
     return "ok";
-});
+}).AllowAnonymous();
 
 app.UseHttpsRedirection();  
 
 
 
 app.Run();
+
+
+public class MyRequirement : IAuthorizationRequirement { }
+
+
+public class MyRequirementHandler : AuthorizationHandler<MyRequirement>
+
+{
+    protected override Task HandleRequirementAsync(AuthorizationHandlerContext context, MyRequirement requirement)
+    {
+        //throw new NotImplementedException();
+        //context.User
+        //context.Succeed(new  MyRequirement());
+
+        return Task.CompletedTask;
+    }
+}
 
 
 
