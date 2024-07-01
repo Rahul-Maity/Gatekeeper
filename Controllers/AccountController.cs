893d9e0f1a69299f276975c1fa6da0fa938a4dd7
Rahul <mrahulmaity623@gmail.com>
2024-07-01 11:59:24 +0530

    Identity authorization

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -1,29 +1,19 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
 namespace Gatekeeper.Controllers
 {
-   
+
     [ApiController]
     public class AccountController : ControllerBase
     {
         [HttpGet("/login")]
-        public IActionResult Login() =>
-            SignIn(new ClaimsPrincipal(
-                new ClaimsIdentity(
-                    new Claim[]
-                    {
-                        new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString()),
-                        new Claim("my_role_claim","admin")
-                    },
-                    "cookie",
-                    nameType:null,
-                    roleType:"my_role_claim"
-                   )
-
-                ),
-                authenticationScheme: "cookie"
-              );
+        public async Task<IActionResult> Login(SignInManager<IdentityUser> signInManager)
+        {
+           await  signInManager.PasswordSignInAsync("test@test.com", "password", false, false);
+            return Ok();
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,13 +1,27 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.IO;
 using System.Runtime.Intrinsics.Arm;
 using System.Security.Claims;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<IdentityDbContext>(c => c.UseInMemoryDatabase("my_db"));
 
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(o =>
+{
+    o.User.RequireUniqueEmail = true;
+    o.Password.RequireDigit = false;
+    o.Password.RequiredLength = 4;
+    o.Password.RequireLowercase = false;
+    o.Password.RequireUppercase = false;
+    o.Password.RequireNonAlphanumeric = false;
+}).AddEntityFrameworkStores<IdentityDbContext>()
+.AddDefaultTokenProviders();
 
 builder.Services.AddAuthentication()
     .AddCookie("cookie");
@@ -26,6 +40,19 @@ app.UseAuthentication();
 
 app.UseAuthorization();
 
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var usrMgr = scope.ServiceProvider.GetRequiredService<UserManager<IdentityUser>>();
+
+    var roleMgr = scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+    await roleMgr.CreateAsync(new IdentityRole() {Name = "admin" });
+    
+    var user = new IdentityUser() { UserName = "test@test.com", Email = "test@test.com" };
+    await usrMgr.CreateAsync(user,password:"password");
+   await  usrMgr.AddToRoleAsync(user, "admin");
+}
 
 
 app.UseHttpsRedirection();
